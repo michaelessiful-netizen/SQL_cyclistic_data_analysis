5b5edf9f88fed8ab082f7ae1dab7eff7dc31ed8c
michaelessiful-netizen <michaelessiful@gmail.com>
2025-11-14 10:40:29 +0000

    bike_share_data with new columns and queries
    Added columns for start time, start hour, start month, end time, and ride duration to bike_share_data. Included SQL queries for data aggregation by ride type, user type, and ride demand.

--- bike_share_analysis .sql
+++ bike_share_analysis .sql
@@ -48,4 +48,159 @@ SET start_hour = HOUR(started_at);
 
 UPDATE bike_share_analysis.bike_share_data
 SET start_day = DAYNAME(started_at);
+UPDATE bike_share_analysis.bike_share_data
+SET start_time = TIME(started_at);
+
+UPDATE bike_share_analysis.bike_share_data
+SET start_hour = HOUR(started_at);
+
+## adding. start_month column and populating it with data from started_at column
+ALTER TABLE bike_share_analysis.bike_share_data
+ADD COLUMN start_month VARCHAR(10);
+UPDATE bike_share_analysis.bike_share_data
+SET start_month = MONTHNAME(started_at);
+
+## creating a new column end_time to help calculate ride duration
+ALTER TABLE bike_share_analysis.bike_share_data
+ADD COLUMN end_time varchar(8);
+
+UPDATE bike_share_analysis.bike_share_data
+SET end_time = TIME(ended_at);
+
+## creating a new column ride_duration 
+ALTER TABLE bike_share_analysis.bike_share_data
+ADD COLUMN ride_duration INT;
+
+UPDATE bike_share_analysis.bike_share_data
+SET ride_duration = 
+	CASE 
+		WHEN timestampdiff(minute, started_at, ended_at) <0
+        THEN timestampdiff(minute, started_at, ended_at) + 1440
+        ELSE timestampdiff(minute, started_at, ended_at)
+    END;    
+    
+## Data Aggregation
+## Average ride length (casual/ members), Bike demand by hour, day, and month, total ride demand (memebrs/casual)    
+# Average ride duration
+SELECT
+    member_casual,
+    AVG(ride_duration) AS avg_ride_length_minutes
+FROM
+    bike_share_analysis.bike_share_data
+GROUP BY
+    member_casual;
+
+## Bike demand per hour    
+SELECT
+    start_hour,
+    COUNT(ride_id) AS total_rides
+FROM
+    bike_share_analysis.bike_share_data
+GROUP BY
+    start_hour
+ORDER BY
+    start_hour;
+
+## Bike demand by day
+SELECT
+    start_day,
+    COUNT(ride_id) AS total_rides
+FROM
+    bike_share_analysis.bike_share_data
+GROUP BY
+    start_day
+ORDER BY
+    FIELD(start_day, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+
+## Bike demand by Month
+SELECT
+    start_month,
+    COUNT(ride_id) AS total_rides
+FROM
+    bike_share_analysis.bike_share_data
+GROUP BY
+    start_month
+ORDER BY
+    FIELD(start_month, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+    
+## total ride demand (member/casual)
+SELECT
+    member_casual,
+    COUNT(ride_id) AS total_rides_by_type
+FROM
+    bike_share_analysis.bike_share_data
+GROUP BY
+    member_casual;
+    
+## Aggregation by user types
+## by hour
+SELECT
+    start_hour,
+    member_casual,
+    COUNT(ride_id) AS total_rides
+FROM
+    bike_share_analysis.bike_share_data
+GROUP BY
+    start_hour, member_casual
+ORDER BY
+    start_hour, member_casual;
+
+## by day 
+SELECT
+    start_day,
+    member_casual,
+    COUNT(ride_id) AS total_rides
+FROM
+    bike_share_analysis.bike_share_data
+GROUP BY
+    start_day, member_casual
+ORDER BY
+    FIELD(start_day, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'), member_casual;
+
+## By Month
+SELECT
+    start_month,
+    member_casual,
+    COUNT(ride_id) AS total_rides
+FROM
+    bike_share_analysis.bike_share_data
+GROUP BY
+    start_month, member_casual
+ORDER BY
+    FIELD(start_month, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'), member_casual;
+    
+## Average ride_duration by day    
+SELECT
+    start_day,
+    member_casual,
+    AVG(ride_duration) AS avg_ride_length_minutes
+FROM
+    bike_share_analysis.bike_share_data
+GROUP BY
+    start_day, member_casual
+ORDER BY
+    FIELD(start_day, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'), member_casual;
+    
+## data aggregation by ride trpe    
+SELECT
+    rideable_type,
+    COUNT(ride_id) AS total_rides
+FROM
+    bike_share_analysis.bike_share_data
+GROUP BY
+    rideable_type
+ORDER BY
+    total_rides DESC;
+
+SELECT
+    start_month,
+    rideable_type,
+    COUNT(ride_id) AS total_rides
+FROM
+    bike_share_analysis.bike_share_data
+GROUP BY
+    start_month, rideable_type
+ORDER BY
+    FIELD(start_month, 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+    total_rides DESC;
             
